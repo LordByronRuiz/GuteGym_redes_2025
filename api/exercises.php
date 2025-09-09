aaab222c6436b9507a9d42080ba2490085482102
LordByronRuiz <byron.ruiz.t1vl@gmail.com>
2025-09-09 16:20:04 -0300

    manejo de errores CORS

--- api/exercises.php
+++ api/exercises.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 include_once __DIR__ . '/../config/database.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
--- api/login.php
+++ api/login.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 include_once __DIR__ . '/../config/database.php';
 
 $data = json_decode(file_get_contents('php://input'), true);
